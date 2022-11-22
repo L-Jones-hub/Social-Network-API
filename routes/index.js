3069ef4b11f40464fed664b809c43e31841e83a9
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-11-22 13:35:17 -0600

    Added index.js to routes folder.

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+router.use("/api", apiRoutes);
+
+router.use((req, res) => {
+  return res.send("Could not fetch that for you. Please try again.");
+});
+
+module.exports = router;
