244c60b2c994983c69e904390800738c2a2bcb4a
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-11-23 11:32:03 -0600

    Added reaction route to api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./UserRoutes");
 const thoughtRoutes = require("./ThoughtRoutes");
-// const reactionRoutes = require('./ReactionRoutes');
+const reactionRoutes = require("./ReactionRoutes");
 
 router.use("/users", userRoutes);
 router.use("/thoughts", thoughtRoutes);
+router.use("/reactions", reactionRoutes);
 
 module.exports = router;
